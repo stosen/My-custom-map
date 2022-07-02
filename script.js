2346365aba86e28b25f1c0431daba9b3ef68a7d8
stosen <shenyijian326@gmail.com>
2022-07-02 17:19:18 -0400

    initialize my Google map

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initialize and add the map
+const initMap = () => {
+  // The location of Big Island
+  const bigIsland = { lat: 19.568694, lng: -155.654428 }
+  // The location of Maui
+  const maui = { lat: 20.814321729941362, lng: -156.32978193167554 }
+  // The location of Kauai
+  const kauai = { lat: 22.107831454996642, lng: -159.52709513605495 }
+  // The location of Oahu
+  const oahu = { lat: 21.454301796912564, lng: -157.99957028630436 }
+}
+
+window.initMap = initMap
